3204cac411d5d2ba433ee4abc1465adf2084a1dd
Nicholas Stebbings <nickolarse@riseup.net>
2024-08-17 12:37:00 +0100

    initial scaffolded web app

---
+++ tests/src/feed/posts/common.ts
@@ -0,0 +1,11 @@
+import {
+  ActionHash,
+  AppBundleSource,
+  fakeActionHash,
+  fakeAgentPubKey,
+  fakeDnaHash,
+  fakeEntryHash,
+  NewEntryAction,
+  Record,
+} from "@holochain/client";
+import { CallableCell } from "@holochain/tryorama";
